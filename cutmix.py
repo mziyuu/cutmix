e3074da9d4bcf4ceae01604e13d6e00fbc11cf44
mziyuu <mziyuu@foxmail.com>
2022-05-30 21:23:06 +0800

    non

--- cutmix.py
+++ cutmix.py
@@ -1,14 +1,27 @@
 import os
 import PIL
 from PIL import Image
-person_img = "1.png"
-background = "frame0001.jpg"
+import random
 
-person_image = Image.open(person_img)
-background_image = Image.open(background)
-new_image = background_image.copy()
-new_image.paste(person_image,)
 
-new_image.show()
+def generate_paste_location(background_size, target_size):
+    pass
 
-print(new_image)
+
+def cutmix(background_image, target_image):
+
+    new_image = background_image.copy()
+    paste_location = generate_paste_location(background_image.size(), target_image.size())
+    new_image.paste(target_image, paste_location)
+    new_image.show()
+
+
+def main():
+    person_img = "1.png"
+    background = "frame0001.jpg"
+    person_image = Image.open(person_img)
+    background_image = Image.open(background)
+    cutmix(background_image, person_image)
+
+if __name__ == "__main__":
+    main()
